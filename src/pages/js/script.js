38140aaecb3d4ba5f07bfb08ac2e64c269b582ea
Polinarka <saira6303@gmail.com>
2020-05-23 13:58:26 +0400

    удалила все ненужные папки..начала пилить дз по js но на сайте ничего не меняктся,чтобы ни писала((

---
+++ src/pages/js/script.js
@@ -0,0 +1,9 @@
+alert ("Start page");
+
+let Email;
+Email = prompt("Email*", );
+console.log("Email: ", Email, " typeof Email: ", typeof Email);
+
+let Pasword;
+Pasword = prompt("Pasword*", );
+console.log("Pasword: ", Pasword, " typeof Pasword: ", typeof Pasword);
